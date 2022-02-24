c5a3b3231c801b29382b9a3b931eb4cb9d449f56
David T Eady <61388628+DevTeady@users.noreply.github.com>
2022-02-25 02:15:18 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
-
 # Change to the Source Directory
 cd $SYNC_PATH
-
 # Set-up ccache
 if [ -z "$CCACHE_SIZE" ]; then
     ccache -M 50G
 else
     ccache -M ${CCACHE_SIZE}
 fi
-
 # Run the Extra Command
 $EXTRA_CMD
-
 # Prepare the Build Environment
 source build/envsetup.sh
-
 # lunch the target
 lunch ${LUNCH_COMBO} || { echo "ERROR: Failed to lunch the target!" && exit 1; }
-
 # Build the Code
 if [ -z "$J_VAL" ]; then
     mka -j$(nproc --all) $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
@@ -27,6 +21,5 @@ elif [ "$J_VAL"="0" ]; then
 else
     mka -j${J_VAL} $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
 fi
-
 # Exit
 exit 0
