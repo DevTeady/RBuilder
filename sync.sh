4f1ac295c6c7b9cc09c7493bf206dae4c4925620
David T Eady <61388628+DevTeady@users.noreply.github.com>
2022-02-25 09:53:08 +0600

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -4,7 +4,7 @@ mkdir -p $SYNC_PATH
 # Change to the Source Directory
 cd $SYNC_PATH
 # Init Repo
-repo init -u $MANIFEST -b $MANIFEST_BRANCH
+repo init --depth=1 -u $MANIFEST -b $MANIFEST_BRANCH
 # Sync the Sources
 repo sync -j$(nproc --all) --force-sync --no-tags --no-clone-bundle
 # Clone Trees
